2e75dc0a2647fff61afbbbb9fd24e16d0d55ad03
willsan0723 <william.santee@gmail.com>
2020-08-16 16:20:34 -0600

    figured out how to generate password set to length specified.

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,16 +12,19 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+var generatePassword = function() {
 var lengthPassword = window.prompt('How many characters will your password be? Passwords require a minimum of 8 characters.');
 if (lengthPassword === "" || lengthPassword === null || lengthPassword < 8) {
   window.alert("You need to provide a valid answer! Please try again.")
   return generatePassword();
 }
+else {
+  lengthPassword = Math.round(lengthPassword);
+}
 //check length only pass through greater than 8 and under 128
 var caseLower = window.confirm('Should your password include lowercase letters?');
 if (caseLower) {
-  caseLower = caseLower
+
 }
 
 var caseUpper = window.confirm('Should your password include uppercase letters?');
@@ -33,7 +36,14 @@ var specialChar = window.confirm('Should your password include special character
 if (specialChar) {
 
 }
-
+var length = lengthPassword,
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+~`",
+    retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
