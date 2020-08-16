14fff19c991ffb059d2ac87192d220b68c3d9984
willsan0723 <william.santee@gmail.com>
2020-08-16 15:56:55 -0600

    added part of the password criterea cose

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,5 +13,27 @@ function writePassword() {
 
 }
 
+var lengthPassword = window.prompt('How many characters will your password be? Passwords require a minimum of 8 characters.');
+if (lengthPassword === "" || lengthPassword === null || lengthPassword < 8) {
+  window.alert("You need to provide a valid answer! Please try again.")
+  return generatePassword();
+}
+//check length only pass through greater than 8 and under 128
+var caseLower = window.confirm('Should your password include lowercase letters?');
+if (caseLower) {
+  caseLower = caseLower
+}
+
+var caseUpper = window.confirm('Should your password include uppercase letters?');
+if (caseUpper) {
+
+}
+
+var specialChar = window.confirm('Should your password include special characters?');
+if (specialChar) {
+
+}
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
